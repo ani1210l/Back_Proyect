b15ed99289de1a1a56cc73253d805b05d978b9af
MichelleYunga <111171604+MichelleYunga@users.noreply.github.com>
2023-07-19 18:02:52 -0500

    factura detalle, factura cabecera

--- SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaCabecera.java
+++ SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaCabecera.java
@@ -1,5 +1,58 @@
 package com.sinfloo.modelo;
 
+
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import javax.persistence.UniqueConstraint;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
+@Entity
+@Table(name = "facturaCabeceras", uniqueConstraints = {@UniqueConstraint(columnNames ={"id_cliente"})})
+
 public class FacturaCabecera {
 
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id_facturaC;
+
+	@ManyToOne
+	@JoinColumn(name = "id_pedidoC")
+	private PedidoCabecera pedidoCabecera;
+
+
+	@OneToMany(mappedBy="facturaCabecera", cascade = CascadeType.ALL)
+	private List<FacturaDetalle> facturaDetalle;
+	
+	
+	
+	
+	public int getId_facturaC() {
+		return id_facturaC;
+	}
+
+	public void setId_facturaC(int id_facturaC) {
+		this.id_facturaC = id_facturaC;
+	}
+	
+
+	
+
+	
 }
--- SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaDetalle.java
+++ SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaDetalle.java
@@ -1,5 +1,58 @@
 package com.sinfloo.modelo;
 
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
 public class FacturaDetalle {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id_facturaD;
+
+	
+	@ManyToOne
+	@JoinColumn(name = "id_pedidoDetalle")
+	private PedidoDetalle pedidoDetalle;
+	
+
+	@ManyToOne
+	@JoinColumn(name = "id_facturaC")
+	private FacturaCabecera facturaCabecera;
+	
+	
+	
+
+	public PedidoDetalle getPedidoDetalle() {
+		return pedidoDetalle;
+	}
+
+	public void setPedidoDetalle(PedidoDetalle pedidoDetalle) {
+		this.pedidoDetalle = pedidoDetalle;
+	}
+
+	public FacturaCabecera getFacturaCabecera() {
+		return facturaCabecera;
+	}
+
+	public void setFacturaCabecera(FacturaCabecera facturaCabecera) {
+		this.facturaCabecera = facturaCabecera;
+	}
+
+	public int getId_facturaD() {
+		return id_facturaD;
+	}
+
+	public void setId_facturaD(int id_facturaD) {
+		this.id_facturaD = id_facturaD;
+	}
+	
+
+	
 }
