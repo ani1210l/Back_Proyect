45028bee6ece708efb101ed9bee804576ccf4541
MichelleYunga <111171604+MichelleYunga@users.noreply.github.com>
2023-07-19 18:07:26 -0500

    factura detalle

--- SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaDetalle.java
+++ SinFloo_app-servicerest-backend-master/src/main/java/com/sinfloo/modelo/FacturaDetalle.java
@@ -2,12 +2,17 @@ package com.sinfloo.modelo;
 
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "facturaDetalles")
 
 public class FacturaDetalle {
 
